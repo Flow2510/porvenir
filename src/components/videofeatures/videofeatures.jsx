3ac42d9a2931c53b06e3dce12ecebf9b834dc80e
Flow2510 <sendra.florian@gmail.com>
2026-03-18 01:47:01 +0100

    oubli video

--- src/components/videofeatures/videofeatures.jsx
+++ src/components/videofeatures/videofeatures.jsx
@@ -1,9 +1,11 @@
 import './videofeatures.scss';
 
+import video from '../../assets/video/interieur.mp4'
+
 export default function VideoFeatures(){
     return(
         <section className='videofeatures'>
-            <video className='videofeatures__video' src="/src/assets/video/interieur.mp4" autoPlay muted loop></video>
+            <video className='videofeatures__video' src={video} autoPlay muted loop></video>
             <div className='videofeatures__content'>
                 <h2 className='videofeatures__content-title'>Title</h2>
                 <p className='videofeatures__content-text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio excepturi perferendis fugiat rem, quo omnis distinctio cum nesciunt? Voluptatem tempora obcaecati fugit magni autem doloribus dolores aspernatur, eum eaque nulla.</p>
